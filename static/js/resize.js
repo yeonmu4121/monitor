6ddd0b9236449a13df2b369396bd6093506cf0ba
Kim Songhyun <yeonmu4121@gmail.com>
2017-10-09 19:35:38 +0900

    Change autoResize()
    Now autoResize function takes id parameter.

--- static/js/resize.js
+++ static/js/resize.js
@@ -1,4 +1,5 @@
-function autoResize(i) {
+function autoResize(id) {
+    var i = document.getElementById(id);
     var iframeHeight = i.contentWindow.document.body.scrollHeight;
     i.height=iframeHeight;
 }
